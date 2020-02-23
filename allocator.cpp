2c435946e06d9abc0c2d49b84385433b4b4fab4e
carrieyin <695685009@qq.com>
2020-02-23 20:40:40 +0800

    修改程序异常

--- allocator.cpp
+++ allocator.cpp
@@ -10,7 +10,7 @@ void * MyAllocator::allocate(size_t size)
 		headOfFreelist = (obj*)malloc(size * MEME_SIZE);
 		obj* p = headOfFreelist;
 		for (int i = 0; i < MEME_SIZE - 1; i++) {
-			p->next = (obj*)p + size;
+			p->next = (obj*)((char*)p + size);
 			p = p->next;
 		}
 		p->next = NULL;
@@ -19,6 +19,7 @@ void * MyAllocator::allocate(size_t size)
 	p = headOfFreelist;
 	headOfFreelist = headOfFreelist->next;
 	return p;
+	
 }
 
 void MyAllocator::dellocate(void * p, size_t size)
